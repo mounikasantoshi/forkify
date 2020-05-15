a2dc5c757a3c708b1fbee79a545aeaf9fb463035
mounika <mounikasantoshi99@gail.com>
2020-05-15 17:15:45 +0530

    initial commit

---
+++ src/test.js
@@ -0,0 +1,2 @@
+console.log('Import module');
+export default 120;
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.export={
+    entry:'./src/index.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'main.js',
+    
+    },
+    devserver: {
+        contentBase:'./dist'
+    }
+};
